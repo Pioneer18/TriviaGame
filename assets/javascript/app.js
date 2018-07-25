325c156ff84f0f20f87d6081d81f2f07025a1de1
Jonathan <sellsj14@gmail.com>
2018-07-24 21:51:50 -0400

    the logic works, loop through object and display the Question and related ansers; shuffle the answers before displaying them

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,9 +16,17 @@ $(document).ready(function(){
         game["Q1"] = ["when was Jonathan D. Sells born?","1992","1989","1994","1990"];
         game["Q2"] = ["where was Jonathan D. Sells born?","Chicago","Tampa","Brandon","St.Petersburg"];
         game["Q3"]= ["what sport is My best?","basketball","baseball","soccer","track & field" ];
+        game["Q4"]= ["Most sold item in Walmart?","toilet paper","bottled water","Cola","bananas" ];
+        game["Q5"]= ["best type of soup?","clam chowder","pho","ramen","chicken noodle" ];
+        game["Q6"]= ["where was President Trump born?","New York","LA","Chicago","DC" ];
+        game["Q7"]= ["What is the Long Jump World Record?","29ft 4.25in","24ft 8in","26ft 7in","28ft 9.75in" ];
+        game["Q8"]= ["which is the best sport; empirically?","basketball","badminton","ultimate frisbee","cricket" ];
+        game["Q9"]= ["which mountain is considered most dangerous to climb in the world?","Mt Everest","Kilimanjaro","Annapurna","K2"];
+        game["Q10"]= ["what is the biggest organism in the world?","blue whale","A Sequoia tree","A honey fungus","sperm whale" ];
+        
     
     //this loop is select each question and insert that Q class question
-    for(i=1; i < 5; i++){
+    for(i=1; i < 11; i++){
         //this should be looping through html elements by id Q(1-4) and filling in the element with the 
         //object property that matches the id. e.g. id Q1.html(property Q1)
         $(".Q"+ i).html(game["Q"+ i][0]);
